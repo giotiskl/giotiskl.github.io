b3b9348183effca5aa08e58bfc35620b4d4f15b3
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-02-12 21:16:59 +0100

    Updates

---
+++ precache-manifest.14e3cae2038dfc4f083b87dc6c9fdb11.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cf5ff594f61e1178ee8",
+    "url": "/static/js/main.4cf5ff59.chunk.js"
+  },
+  {
+    "revision": "8f46e749c9b2b475c881",
+    "url": "/static/js/1.8f46e749.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "53badbe682d1f07914e84cec8eab7a36",
+    "url": "/index.html"
+  }
+];
